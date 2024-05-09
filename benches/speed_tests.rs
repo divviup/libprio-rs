8e563f0a684dd8dee2ed19bb734d1f7ebcc1ea2a
David Cook <dcook@divviup.org>
2024-05-09 13:08:07 -0500

    Stop using iter_batched in Poplar1 benchmarks
    Using Bencher::iter_batched() is only necessary if the benchmarked
    routine needs to consume its output, which is not the case here. This
    change lets us avoid unnecessarily repeating identical setup
    computations.

--- benches/speed_tests.rs
+++ benches/speed_tests.rs
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MPL-2.0
 
-use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 #[cfg(feature = "experimental")]
-use criterion::{BatchSize, Throughput};
+use criterion::Throughput;
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 #[cfg(feature = "experimental")]
 use fixed::types::{I1F15, I1F31};
 #[cfg(feature = "experimental")]
@@ -715,19 +715,14 @@ fn poplar1(c: &mut Criterion) {
             let vdaf = Poplar1::new_turboshake128(size);
             let mut rng = StdRng::seed_from_u64(RNG_SEED);
             let nonce = rng.gen::<[u8; 16]>();
+            let bits = iter::repeat_with(|| rng.gen())
+                .take(size)
+                .collect::<Vec<bool>>();
+            let measurement = IdpfInput::from_bools(&bits);
 
-            b.iter_batched(
-                || {
-                    let bits = iter::repeat_with(|| rng.gen())
-                        .take(size)
-                        .collect::<Vec<bool>>();
-                    IdpfInput::from_bools(&bits)
-                },
-                |measurement| {
-                    vdaf.shard(&measurement, &nonce).unwrap();
-                },
-                BatchSize::SmallInput,
-            );
+            b.iter(|| {
+                vdaf.shard(&measurement, &nonce).unwrap();
+            });
         });
     }
     group.finish();
@@ -738,57 +733,43 @@ fn poplar1(c: &mut Criterion) {
         group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
             let vdaf = Poplar1::new_turboshake128(size);
             let mut rng = StdRng::seed_from_u64(RNG_SEED);
+            let verify_key: [u8; 16] = rng.gen();
+            let nonce: [u8; 16] = rng.gen();
 
-            b.iter_batched(
-                || {
-                    let verify_key: [u8; 16] = rng.gen();
-                    let nonce: [u8; 16] = rng.gen();
-
-                    // Parameters are chosen to match Chris Wood's experimental setup:
-                    // https://github.com/chris-wood/heavy-hitter-comparison
-                    let (measurements, prefix_tree) = generate_zipf_distributed_batch(
-                        &mut rng, // rng
-                        size,     // bits
-                        10,       // threshold
-                        1000,     // number of measurements
-                        128,      // Zipf support
-                        1.03,     // Zipf exponent
-                    );
-
-                    // We are benchmarking preparation of a single report. For this test, it doesn't matter
-                    // which measurement we generate a report for, so pick the first measurement
-                    // arbitrarily.
-                    let (public_share, input_shares) =
-                        vdaf.shard(&measurements[0], &nonce).unwrap();
-
-                    // For the aggregation paramter, we use the candidate prefixes from the prefix tree
-                    // for the sampled measurements. Run preparation for the last step, which ought to
-                    // represent the worst-case performance.
-                    let agg_param =
-                        Poplar1AggregationParam::try_from_prefixes(prefix_tree[size - 1].clone())
-                            .unwrap();
-
-                    (
-                        verify_key,
-                        nonce,
-                        agg_param,
-                        public_share,
-                        input_shares.into_iter().next().unwrap(),
-                    )
-                },
-                |(verify_key, nonce, agg_param, public_share, input_share)| {
-                    vdaf.prepare_init(
-                        &verify_key,
-                        0,
-                        &agg_param,
-                        &nonce,
-                        &public_share,
-                        &input_share,
-                    )
-                    .unwrap();
-                },
-                BatchSize::SmallInput,
+            // Parameters are chosen to match Chris Wood's experimental setup:
+            // https://github.com/chris-wood/heavy-hitter-comparison
+            let (measurements, prefix_tree) = generate_zipf_distributed_batch(
+                &mut rng, // rng
+                size,     // bits
+                10,       // threshold
+                1000,     // number of measurements
+                128,      // Zipf support
+                1.03,     // Zipf exponent
             );
+
+            // We are benchmarking preparation of a single report. For this test, it doesn't matter
+            // which measurement we generate a report for, so pick the first measurement
+            // arbitrarily.
+            let (public_share, input_shares) = vdaf.shard(&measurements[0], &nonce).unwrap();
+            let input_share = input_shares.into_iter().next().unwrap();
+
+            // For the aggregation paramter, we use the candidate prefixes from the prefix tree for
+            // the sampled measurements. Run preparation for the last step, which ought to represent
+            // the worst-case performance.
+            let agg_param =
+                Poplar1AggregationParam::try_from_prefixes(prefix_tree[size - 1].clone()).unwrap();
+
+            b.iter(|| {
+                vdaf.prepare_init(
+                    &verify_key,
+                    0,
+                    &agg_param,
+                    &nonce,
+                    &public_share,
+                    &input_share,
+                )
+                .unwrap();
+            });
         });
     }
     group.finish();
